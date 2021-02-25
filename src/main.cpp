1e9c92b1d20a64a060cb1fcfccb34b0e8980ed07
Davide Risaliti <davide.risaliti@caditgroup.com>
2021-02-25 14:23:23 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <cstdio>
+#include <string>
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 800;
+
+SDL_Surface *loadSurface(SDL_Surface *surface, const std::string &path) {
+    //The final optimized image
+    SDL_Surface *optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+    if (loadedSurface == NULL) {
+        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+    } else {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface(loadedSurface, surface->format, 0);
+        if (optimizedSurface == NULL) {
+            printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return optimizedSurface;
+}
+
+int main(int argc, char *args[]) {
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    //The window we'll be rendering to
+    SDL_Window *window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface *screenSurface = NULL;
+
+    //Create window
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    //Get window surface
+    screenSurface = SDL_GetWindowSurface(window);
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    // Prova
+    SDL_Surface *prova = loadSurface(screenSurface, "prova.png");
+
+    //While application is running
+    while (!quit) {
+        //Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            //User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+        //Apply the image
+        SDL_BlitSurface(prova, NULL, screenSurface, NULL);
+
+        //Update the surface
+        SDL_UpdateWindowSurface(window);
+    }
+
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
